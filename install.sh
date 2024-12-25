9955f22745e337d20072b9ae977ac8b4664c256e
Thiago Negri <evohunz@gmail.com>
2024-12-25 13:45:52 -0300

    cleanup

--- install.sh
+++ install.sh
@@ -9,17 +9,17 @@ esac
 
 # Install fzf
 if [ ! -d "$HOME/.fzf" ]; then
-    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    git clone --depth 1 git@github.com:junegunn/fzf.git ~/.fzf
     ~/.fzf/install --key-bindings --completion --no-update-rc
 fi
 
-# Setup .zshrc
+# Setup zsh
 [ ! -d "$HOME/.tmp" ] && mkdir $HOME/.tmp
 cat zshrc > ~/.zshrc
 [[ "$os" = "mac" ]] && cat zshrc_mac >> ~/.zshrc
 [[ "$os" = "win" ]] && cat zshrc_win >> ~/.zshrc
 
-# Setup .vimrc
+# Setup vim
 [ ! -d "$HOME/.vim/pack" ] && mkdir -p $HOME/.vim/pack/downloads/{start,opt}
 cd "$HOME/.vim/pack/downloads/opt"
 [ ! -d "./fzf.vim" ]             && git clone --depth 1 git@github.com:thiago-negri/fzf.vim.git
@@ -37,7 +37,7 @@ cat vimrc >> ~/.vimrc
 [[ "$os" = "mac" ]] && cat vimrc_mac >> ~/.vimrc
 [[ "$os" = "win" ]] && cat vimrc_win >> ~/.vimrc
 
-# Install LSPs
+# Install vim LSPs
 [ ! -d "$HOME/.vim/lsps" ] && mkdir -p $HOME/.vim/lsps
 cd $HOME/.vim/lsps
 [ ! -d "./node_modules/typescript-language-server" ] && npm i typescript-language-server
