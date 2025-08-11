e4c5c97eb1d85f22ae8b633335c3eb92c266e236
Thiago Negri <evohunz@gmail.com>
2025-08-10 21:56:39 -0300

    add elixir and erlang to linux path

--- install.sh
+++ install.sh
@@ -34,41 +34,42 @@ fi
 cat zshrc > $HOME/.zshrc
 [[ "$os" = "mac" ]] && cat zshrc_mac >> $HOME/.zshrc
 [[ "$os" = "win" ]] && cat zshrc_win >> $HOME/.zshrc
+[[ "$os" = "linux" ]] && cat zshrc_linux >> $HOME/.zshrc
 
 
 #
 # vim
 #
 
-# vim packages
-[ ! -d "$HOME/.vim/pack" ] && mkdir -p $HOME/.vim/pack/downloads/{start,opt}
-cd "$HOME/.vim/pack/downloads/opt"
-# [ ! -d "./fzf.vim" ]             && git clone --depth 1 git@github.com:thiago-negri/fzf.vim.git
-[ ! -d "./fzf.vim" ]             && git clone --depth 1 git@github.com:junegunn/fzf.vim.git
-[ ! -d "./lsp" ]                 && git clone --depth 1 git@github.com:yegappan/lsp.git
-[ ! -d "./vim-commentary" ]      && git clone --depth 1 git@github.com:tpope/vim-commentary.git
-[ ! -d "./vim-dark" ]            && git clone --depth 1 git@github.com:thiago-negri/vim-dark.git
-[ ! -d "./vim-easymotion" ]      && git clone --depth 1 git@github.com:easymotion/vim-easymotion.git
-[ ! -d "./vim-fugitive" ]        && git clone --depth 1 git@github.com:tpope/vim-fugitive.git
-[ ! -d "./vim-graphql" ]         && git clone --depth 1 git@github.com:jparise/vim-graphql.git
-[ ! -d "./vim-highlightedyank" ] && git clone --depth 1 git@github.com:machakann/vim-highlightedyank.git
-[ ! -d "./vim-sleuth" ]          && git clone --depth 1 git@github.com:tpope/vim-sleuth.git
-[ ! -d "./vim-surround" ]        && git clone --depth 1 git@github.com:tpope/vim-surround.git
-[ ! -d "./vim-vinegar" ]         && git clone --depth 1 git@github.com:tpope/vim-vinegar.git
-cd - >/dev/null
+# # vim packages
+# [ ! -d "$HOME/.vim/pack" ] && mkdir -p $HOME/.vim/pack/downloads/{start,opt}
+# cd "$HOME/.vim/pack/downloads/opt"
+# # [ ! -d "./fzf.vim" ]             && git clone --depth 1 git@github.com:thiago-negri/fzf.vim.git
+# [ ! -d "./fzf.vim" ]             && git clone --depth 1 git@github.com:junegunn/fzf.vim.git
+# [ ! -d "./lsp" ]                 && git clone --depth 1 git@github.com:yegappan/lsp.git
+# [ ! -d "./vim-commentary" ]      && git clone --depth 1 git@github.com:tpope/vim-commentary.git
+# [ ! -d "./vim-dark" ]            && git clone --depth 1 git@github.com:thiago-negri/vim-dark.git
+# [ ! -d "./vim-easymotion" ]      && git clone --depth 1 git@github.com:easymotion/vim-easymotion.git
+# [ ! -d "./vim-fugitive" ]        && git clone --depth 1 git@github.com:tpope/vim-fugitive.git
+# [ ! -d "./vim-graphql" ]         && git clone --depth 1 git@github.com:jparise/vim-graphql.git
+# [ ! -d "./vim-highlightedyank" ] && git clone --depth 1 git@github.com:machakann/vim-highlightedyank.git
+# [ ! -d "./vim-sleuth" ]          && git clone --depth 1 git@github.com:tpope/vim-sleuth.git
+# [ ! -d "./vim-surround" ]        && git clone --depth 1 git@github.com:tpope/vim-surround.git
+# [ ! -d "./vim-vinegar" ]         && git clone --depth 1 git@github.com:tpope/vim-vinegar.git
+# cd - >/dev/null
 
-# vimrc
-cat vimrc > $HOME/.vimrc
-[[ "$os" = "mac" ]] && cat vimrc_mac >> $HOME/.vimrc
-[[ "$os" = "win" ]] && cat vimrc_win >> $HOME/.vimrc
+# # vimrc
+# cat vimrc > $HOME/.vimrc
+# [[ "$os" = "mac" ]] && cat vimrc_mac >> $HOME/.vimrc
+# [[ "$os" = "win" ]] && cat vimrc_win >> $HOME/.vimrc
 
-# vim filetypes
-mkdir -p $HOME/.vim/ftplugin
-cp vimrc_gitcommit $HOME/.vim/ftplugin/gitcommit.vim
+# # vim filetypes
+# mkdir -p $HOME/.vim/ftplugin
+# cp vimrc_gitcommit $HOME/.vim/ftplugin/gitcommit.vim
 
-# vim LSPs
-[ ! -d "$HOME/.vim/lsps" ] && mkdir -p $HOME/.vim/lsps
-cd $HOME/.vim/lsps
-[ ! -d "./node_modules/typescript-language-server" ] && npm i typescript-language-server
-cd - >/dev/null
+# # vim LSPs
+# [ ! -d "$HOME/.vim/lsps" ] && mkdir -p $HOME/.vim/lsps
+# cd $HOME/.vim/lsps
+# [ ! -d "./node_modules/typescript-language-server" ] && npm i typescript-language-server
+# cd - >/dev/null
 
