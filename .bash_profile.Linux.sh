c2f2decf7a485962a16647fbb04e079c1ba38b95
thiago-negri <evohunz@gmail.com>
2025-10-07 01:25:32 -0300

    dbus session on login shells; runsvdir on xinitrc

--- .bash_profile.Linux.sh
+++ .bash_profile.Linux.sh
@@ -1,7 +1,22 @@
+# Make sure we're in a DBus session. This is the very first thing that
+# bash will execute on login shells, so we exec into a new DBus session
+# that will exec into a new bash login shell. The new bash login shell
+# will have the envvar set and .bash_profile will continue to load
+# normally. If this is misconfigured, it will keep exec'ing into itself.
+if [ -z "$DBUS_SESSION_BUS_ADDRESS$DID_EXEC_DBUS_RUN_SESSION" ]; then
+    export DID_EXEC_DBUS_RUN_SESSION=yes
+    exec dbus-run-session -- bash -l
+fi
+
+# The current WezTerm release has an odd bug that makes it have a very
+# slot startup sometimes because of a weird interaction with Xorg.
+# It's already fixed, but they did not release a new version yet. This
+# is a version built from their repo.
+export TERMINAL="$HOME/projects/wezterm/target/release/wezterm"
+
 PATH="$PATH:$HOME/.fzf/bin"
 PATH="$PATH:$HOME/.local/bin"
 PATH="$PATH:$HOME/.cargo/bin"
 export PATH
 
-XDEB_PKGROOT="$HOME/.config/xdeb"
-export XDEB_PKGROOT
+export XDEB_PKGROOT="$HOME/.config/xdeb"
