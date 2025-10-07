c1c044f7e4b10c6fa156c5133f9616d2d8d8cb12
thiago-negri <evohunz@gmail.com>
2025-10-07 20:00:29 -0300

    more info on panel

--- .config/herbstluftwm/panel.sh
+++ .config/herbstluftwm/panel.sh
@@ -1,164 +1,156 @@
 #!/usr/bin/env bash
 
-trap 'kill $(jobs -p)' TERM
+# we start quite a few bg processes, clean them up when we die
+trap 'kill $(jobs -p) 2>/dev/null' INT TERM
 
-quote() {
-	local q="$(printf '%q ' "$@")"
-	printf '%s' "${q% }"
-}
+monitor=$1
 
 ### dependencies, if missing this will not work as expected
-textwidth="textwidth"
-awk="awk"
+hc=herbstclient
+textwidth=textwidth
+awk=awk
 xkblayoutnotify="$HOME/projects/xkblayoutnotify/xkblayoutnotify"
-voldown="wpctl set-volume @DEFAULT_SINK@ 1%- -l 1.0"
-volup="wpctl set-volume @DEFAULT_SINK@ 1%+ -l 1.0"
+voldown='wpctl set-volume @DEFAULT_SINK@ 3%- -l 1.0'
+volup='wpctl set-volume @DEFAULT_SINK@ 3%+ -l 1.0'
+mute='wpctl set-mute @DEFAULT_SINK@ toggle'
 volnotify="$HOME/projects/utils/volnotify.sh"
-mute="wpctl set-mute @DEFAULT_SINK@ toggle"
-xkb_switch="xkb-switch"
+xkb_switch=xkb-switch
 
-# if [[ -f /usr/lib/bash/sleep ]]; then
-#     # load and enable 'sleep' builtin (does not support unit suffixes: h, m, s!)
-#     # requires pkg 'bash-builtins' on debian; included in 'bash' on arch.
-#     enable -f /usr/lib/bash/sleep sleep
-# fi
+### theme
+font='-*-fixed-medium-*-*-*-20-*-*-*-*-*-*-*'
+c0='#101010'
+c1='#303030'
+c2='#606060'
+c3='#a0a0a0'
+c4='#e0e0e0'
 
-hc_quoted="$(quote "${herbstclient_command[@]:-herbstclient}")"
-hc() { "${herbstclient_command[@]:-herbstclient}" "$@" ;}
-monitor=${1:-0}
-geometry=( $(hc monitor_rect "$monitor") )
-if [ -z "$geometry" ] ;then
-    echo "Invalid monitor $monitor"
-    exit 1
-fi
-# geometry has the format X Y W H
-gx=${geometry[0]}
-gy=${geometry[1]}
-gw=${geometry[2]}
-gh=${geometry[3]}
-x=$gx
-panel_width=$gw
-panel_height=24
-y=$(($gh - $panel_height))
-font="-*-fixed-medium-*-*-*-20-*-*-*-*-*-*-*"
-# extract colors from hlwm and omit alpha-value
-bgcolor=$(hc get frame_border_normal_color|sed 's,^\(\#[0-9a-f]\{6\}\)[0-9a-f]\{2\}$,\1,')
-selbg=$(hc get window_border_active_color|sed 's,^\(\#[0-9a-f]\{6\}\)[0-9a-f]\{2\}$,\1,')
-selfg='#101010'
+### rect
+read -r mx my mw mh <<< $($hc monitor_rect "$monitor")
+pw=$mw
+ph=24
+px=$mx
+py=$(($mh - $ph))
 
-####
-# textwidth="textwidth"
-# if which textwidth &> /dev/null ; then
-#     textwidth="textwidth";
-# elif which dzen2-textwidth &> /dev/null ; then
-#     textwidth="dzen2-textwidth";
-# elif which xftwidth &> /dev/null ; then # For guix
-#     textwidth="xftwidth";
-# else
-#     echo "This script requires the textwidth tool of the dzen2 project."
-#     exit 1
-# fi
-
-# if awk -Wv 2>/dev/null | head -1 | grep -q '^mawk'; then
-#     # mawk needs "-W interactive" to line-buffer stdout correctly
-#     # http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=593504
-#     uniq_linebuffered() {
-#       awk -W interactive '$0 != l { print ; l=$0 ; fflush(); }' "$@"
-#     }
-# else
-    # other awk versions (e.g. gawk) issue a warning with "-W interactive", so
-    # we don't want to use it there.
-    uniq_linebuffered() {
-      $awk '$0 != l { print ; l=$0 ; fflush(); }' "$@"
-    }
-# fi
-
+### pad hlwm screen space to reserve space for the panel
 padup=0
 padright=0
-paddown=$(($panel_height + 0))
+paddown=$(($ph + 0))
 padleft=0
+$hc pad $monitor $padup $padright $paddown $padleft
 
-hc pad $monitor $padup $padright $paddown $padleft
+# ignore repeated lines
+uniq_linebuffered() {
+  $awk '$0 != l { print ; l=$0 ; fflush(); }' "$@"
+}
 
 {
     ### Event generator ###
-    # based on different input data (mpc, date, hlwm hooks, ...) this generates events, formed like this:
-    #   <eventname>\t<data> [...]
-    # e.g.
-    #   date    ^fg(#efefef)18:33^fg(#909090), 2013-10-^fg(#efefef)29
+    # <eventname>\t<data1>\t<data2>\t<...>\n
 
+    # current date and time, 1s
     while true ; do
-        printf 'date\t^fg(#efefef)%(%A)T^fg(#909090), ^fg(#efefef)%(%d)T^fg(#909090)/%(%m/%Y)T ^fg(#efefef)%(%H:%M)T\n'
+        # date weekday year month day hour minute
+        printf 'date\t%(%A)T\t%(%Y)T\t%(%m)T\t%(%d)T\t%(%H)T\t%(%M)T\n'
+        sleep 1 || break
+    done > >(uniq_linebuffered) &
+
+    # ethernet status, 5s
+    while true ; do
+        # enp status ip
+        IFS=' /' read -r _ st addr _ <<< $(ip -br addr show enp109s0)
+        printf "enp\t$st\t$addr\n"
         sleep 5 || break
     done > >(uniq_linebuffered) &
+
+    # wifi status, 5s
+    while true ; do
+        # wlp status ip
+        IFS=' /' read -r _ st addr _ <<< $(ip -br addr show wlp0s20f3)
+        if [ "$st" = "UP" ]; then
+            wlp_name=$(nmcli connection show --active | grep wlp0s20f3 | awk '{print $1}')
+        else
+            wlp_name=""
+        fi
+        printf "wlp\t$st\t$addr\t$wlp_name\n"
+        sleep 5 || break
+    done > >(uniq_linebuffered) &
+
+    # keyboard layout
     $xkblayoutnotify &
+
+    # volume
     $volnotify &
-    hc --idle
+
+    # hlwm events, this will block
+    $hc --idle
+
     kill $(jobs -p)
 } 2>/dev/null | {
-    IFS=$'\t' read -ra tags <<< "$(hc tag_status $monitor)"
+    separator="^bg()^fg($c1) | "
+
+    # starting values
+    IFS=$'\t' read -ra tags <<< "$($hc tag_status $monitor)"
     visible=true
     date=""
     xkb_layout=""
-    volume="0"
-    # windowtitle=""
+    volume=" ^fg($c2)V ^fg($c4)????? "
+    enp=""
+    wlp=""
+    windowtitle=""
+
     while true ; do
 
         ### Output ###
-        # This part prints dzen data based on the _previous_ data handling run,
-        # and then waits for the next event to happen.
 
-        separator="^bg()^fg(#303030)|"
-        # draw tags
+        # tags
         for i in "${tags[@]}" ; do
+            # color
             case ${i:0:1} in
                 '#') # focus
-                    echo -n "^bg($selbg)^fg($selfg)"
+                    echo -n "^bg($c3)^fg($c0)"
                     ;;
                 '+') # shown in this monitor, but not focus
-                    echo -n "^bg(#9CA668)^fg(#141414)"
+                    echo -n "^bg($c1)^fg($c0)"
                     ;;
                 ':') # not shown, has windows
-                    echo -n "^bg()^fg(#ffffff)"
+                    echo -n "^bg()^fg($c3)"
                     ;;
                 '!') # urgent
-                    echo -n "^bg(#FF0675)^fg(#141414)"
+                    echo -n "^bg()^fg($c3)"
                     ;;
                 *)
-                    echo -n "^bg()^fg(#ababab)"
+                    echo -n "^bg()^fg($c1)"
                     ;;
             esac
-            echo -n "^ca(1,$hc_quoted focus_monitor \"$monitor\" && "
-            echo -n "$hc_quoted use \"${i:1}\") ${i:1} ^ca()"
+            # clickable tag number
+            echo -n "^ca(1,$hc chain . focus_monitor \"$monitor\" . use \"${i:1}\") ${i:1} ^ca()"
         done
+
         echo -n "$separator"
-        # echo -n "^bg()^fg() ${windowtitle//^/^^}"
-        # small adjustments
-        clickable_xkb_layout="^fg(#a0a0a0)^ca(1,$xkb_switch -n) $xkb_layout ^ca()"
-        if [ "$volume" = "MUTED" ]; then
-            volcolor="#d77070"
-        else
-            volcolor="#a0a0a0"
-        fi
-        volume_bar="^fg($volcolor)^ca(1,$mute) $volume ^ca()"
-        right="$volume_bar$separator^bg()$clickable_xkb_layout$separator^bg() $date"
-        right_text_only=$(echo -n "$right" | sed 's.\^[^(]*([^)]*)..g')
-        # get width of right aligned text.. and add some space..
-        width=$($textwidth "$font" "$right_text_only ")
-        echo -n "^pa($(($panel_width - $width)))$right"
+
+        echo -n "^bg()^fg($c3) ${windowtitle//^/^^}"
+
+        # keyboard layout
+        dzen_xkblayout="^fg($c3)^ca(1,$xkb_switch -n) $xkb_layout ^ca()"
+
+        # volume color
+        # clickable volume indicator
+        dzen_vol="^ca(1,$mute)$volume^ca()"
+
+        # full right bar
+        right="$wlp$separator$enp$separator$dzen_vol$separator$dzen_xkblayout$separator$date"
+        right_text=$(echo -n "$right" | sed 's.\(\^[^(]*([^)]*)\)\|MUTED\|?????..g')
+        # width of right aligned text
+        width=$($textwidth "$font" "$right_text")
+        echo -n "^pa($(($pw - $width - 60)))$right"
         echo
 
+
         ### Data handling ###
-        # This part handles the events generated in the event loop, and sets
-        # internal variables based on them. The event and its arguments are
-        # read into the array cmd, then action is taken depending on the event
-        # name.
-        # "Special" events (quit_panel/togglehidepanel/reload) are also handled
-        # here.
 
         # wait for next event
         if ! IFS=$'\t' read -ra cmd; then
-            kill $(jobs -p)
+            kill $(jobs -p) 2>/dev/null
             break
         fi
 
@@ -166,31 +158,51 @@ hc pad $monitor $padup $padright $paddown $padleft
         case "${cmd[0]}" in
             ## custom events
             date)
-                date="${cmd[@]:1}"
+                date=" ^fg($c3)${cmd[1]}^fg($c2), ${cmd[2]}-${cmd[3]}-^fg($c3)${cmd[4]} ${cmd[5]}:${cmd[6]} "
                 ;;
 
             xkb_layout)
-                if [ "${cmd[1]}" = "0" ] ; then
-                    xkb_layout="us"
+                if [ "${cmd[1]}" = "0" ]; then
+                    xkb_layout="^fg($c2)K ^fg($c3)us"
                 else
-                    xkb_layout="intl"
+                    xkb_layout="^fg($c2)K ^fg($c3)intl"
                 fi
                 ;;
 
             vol)
-                volume="${cmd[1]}"
+                if [ "${cmd[1]}" = "MUTED" ]; then
+                    volume=" ^fg($c2)V ^fg($c3)MUTED "
+                else
+                    volfil=$(( ${cmd[1]} / 2 ))
+                    volemp=$(( 50 - (${cmd[1]} / 2) ))
+                    volume=" ^fg($c2)V ^fg($c3)^r(${volfil}x10)^fg($c1)^r(${volemp}x10) "
+                fi
+                ;;
+
+            enp)
+                case "${cmd[1]}" in
+                    UP) enp=" ^fg($c2)E ^fg($c3)${cmd[2]} ";;
+                    * ) enp=" ^fg($c2)E ^fg($c3)${cmd[1]} ";;
+                esac
+                ;;
+
+            wlp)
+                case "${cmd[1]}" in
+                    UP) wlp=" ^fg($c2)W ^fg($c3)${cmd[2]} ^fg($c2)${cmd[3]} ";;
+                    * ) wlp=" ^fg($c2)W ^fg($c3)${cmd[1]} ";;
+                esac
                 ;;
 
             ## events from "herbstclient --idle"
             tag*)
-                IFS=$'\t' read -ra tags <<< "$(hc tag_status $monitor)"
+                IFS=$'\t' read -ra tags <<< "$($hc tag_status $monitor)"
                 ;;
             quit_panel)
-                kill $(jobs -p)
+                kill $(jobs -p) 2>/dev/null
                 exit
                 ;;
             togglehidepanel)
-                currentmonidx=$(hc list_monitors | sed -n '/\[FOCUS\]$/s/:.*//p')
+                currentmonidx=$($hc list_monitors | sed -n '/\[FOCUS\]$/s/:.*//p')
                 if [ "${cmd[1]}" -ne "$monitor" ] ; then
                     continue
                 fi
@@ -200,19 +212,19 @@ hc pad $monitor $padup $padright $paddown $padleft
                 echo "^togglehide()"
                 if $visible ; then
                     visible=false
-                    hc pad $monitor 0 0 0 0
+                    $hc pad $monitor 0 0 0 0
                 else
                     visible=true
-                    hc pad $monitor $padup $padright $paddown $padleft
+                    $hc pad $monitor $padup $padright $paddown $padleft
                 fi
                 ;;
             reload)
-                kill $(jobs -p)
+                kill $(jobs -p) 2>/dev/null
                 exit
                 ;;
-            # focus_changed|window_title_changed)
-            #     windowtitle="${cmd[@]:2}"
-            #     ;;
+            focus_changed|window_title_changed)
+                windowtitle="${cmd[@]:2}"
+                ;;
         esac
     done
 
@@ -220,6 +232,6 @@ hc pad $monitor $padup $padright $paddown $padleft
     # After the data is gathered and processed, the output of the previous block
     # gets piped to dzen2.
 
-} | dzen2 -w $panel_width -x $x -y $y -fn "$font" -h $panel_height \
+} | dzen2 -w $pw -h $ph -x $px -y $py -fn "$font" \
     -e "button3=;button4=exec:$volup;button5=exec:$voldown" \
-    -ta l -bg "$bgcolor" -fg '#efefef'
+    -ta l -bg "$c0" -fg "$c3"
