0c2fd8dab4abb197680e125ea24e7564a7552891
thiago-negri <evohunz@gmail.com>
2026-07-08 07:35:21 -0300

    remove slack from hammerspoon

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -5,7 +5,3 @@ end)
 hs.hotkey.bind({"cmd"}, "2", function()
   hs.application.launchOrFocus("Firefox")
 end)
-
-hs.hotkey.bind({"cmd"}, "3", function()
-  hs.application.launchOrFocus("Slack")
-end)
